ce0d130af1100c1b487bd0069715bb1f92fd0a5f
LaMonica_Arnold <ltarnold03@yahoo.com>
2021-05-14 13:48:25 -0500

    add event listeners

---
+++ assets/js/script.js
@@ -0,0 +1,236 @@
+/* Button objects begin */
+var buttonEl = document.querySelector("#clock");
+console.log(buttonEl);
+var buttonEl = document.querySelector("#answer-1");
+console.log(buttonEl)
+var buttonEl = document.querySelector("#answer-2");
+console.log(buttonEl)
+var buttonEl = document.querySelector("#answer-3");
+console.log(buttonEl)
+var buttonEl = document.querySelector("#answer-4");
+console.log(buttonEl)
+var buttonEl = document.querySelector("#answer-5");
+console.log(buttonEl)
+var buttonEl = document.querySelector("#answer-6");
+console.log(buttonEl)
+var buttonEl = document.querySelector("#answer-7");
+console.log(buttonEl)
+var buttonEl = document.querySelector("#answer-8");
+console.log(buttonEl)
+var buttonEl = document.querySelector("#answer-9");
+console.log(buttonEl)
+var buttonEl = document.querySelector("#answer-10");
+console.log(buttonEl)
+var buttonEl = document.querySelector("#answer-11");
+console.log(buttonEl)
+var buttonEl = document.querySelector("#answer-11");
+console.log(buttonEl)
+var buttonEl = document.querySelector("#answer-13");
+console.log(buttonEl)
+var buttonEl = document.querySelector("#answer-14");
+console.log(buttonEl)
+var buttonEl = document.querySelector("#answer-15");
+console.log(buttonEl)
+var buttonEl = document.querySelector("#answer-16");
+console.log(buttonEl)
+var buttonEl = document.querySelector("#answer-17");
+console.log(buttonEl)
+var buttonEl = document.querySelector("#answer-18");
+console.log(buttonEl)
+var buttonEl = document.querySelector("#answer-19");
+console.log(buttonEl)
+var buttonEl = document.querySelector("#answer-20");
+console.log(buttonEl)
+/* Button objects end */
+
+/* Event Listener Begins */
+var buttonEl = document.querySelector("#clock");
+var btnEl = document.querySelector("timer");
+
+buttonEl.addEventListener("click", function() {
+    var answerEl = document.createElement("button");
+    answerEl.className = "timer";
+    console.log("timer has been clicked")
+});
+
+var buttonEl = document.querySelector("#answer-1");
+var btnEl = document.querySelector("btn-1");
+
+buttonEl.addEventListener("click", function() {
+    var answerEl = document.createElement("button");
+    answerEl.className = "btn-1";
+    console.log("btn-1 has been clicked")
+});
+
+var buttonEl = document.querySelector("#answer-2");
+var btnEl = document.querySelector("btn-2");
+
+buttonEl.addEventListener("click", function() {
+    var answerEl = document.createElement("button");
+    answerEl.className = "btn-2";
+    console.log("btn-2 has been clicked")
+});
+
+var buttonEl = document.querySelector("#answer-3");
+var btnEl = document.querySelector("btn-3");
+
+buttonEl.addEventListener("click", function() {
+    var answerEl = document.createElement("button");
+    answerEl.className = "btn-3";
+    console.log("btn-3 has been clicked")
+});
+
+var buttonEl = document.querySelector("#answer-4");
+var btnEl = document.querySelector("btn-4");
+
+buttonEl.addEventListener("click", function() {
+    var answerEl = document.createElement("button");
+    answerEl.className = "btn-4";
+    console.log("btn-4 has been clicked")
+});
+
+var buttonEl = document.querySelector("#answer-5");
+var btnEl = document.querySelector("btn-5");
+
+buttonEl.addEventListener("click", function() {
+    var answerEl = document.createElement("button");
+    answerEl.className = "btn-5";
+    console.log("btn-5 has been clicked")
+});
+
+var buttonEl = document.querySelector("#answer-6");
+var btnEl = document.querySelector("btn-6");
+
+buttonEl.addEventListener("click", function() {
+    var answerEl = document.createElement("button");
+    answerEl.className = "btn-6";
+    console.log("btn-6 has been clicked")
+});
+
+var buttonEl = document.querySelector("#answer-7");
+var btnEl = document.querySelector("btn-7");
+
+buttonEl.addEventListener("click", function() {
+    var answerEl = document.createElement("button");
+    answerEl.className = "btn-7";
+    console.log("btn-7 has been clicked")
+});
+
+var buttonEl = document.querySelector("#answer-8");
+var btnEl = document.querySelector("btn-8");
+
+buttonEl.addEventListener("click", function() {
+    var answerEl = document.createElement("button");
+    answerEl.className = "btn-8";
+    console.log("btn-8 has been clicked")
+});
+
+var buttonEl = document.querySelector("#answer-9");
+var btnEl = document.querySelector("btn-9");
+
+buttonEl.addEventListener("click", function() {
+    var answerEl = document.createElement("button");
+    answerEl.className = "btn-9";
+    console.log("btn-9 has been clicked")
+});
+
+var buttonEl = document.querySelector("#answer-10");
+var btnEl = document.querySelector("btn-10");
+
+buttonEl.addEventListener("click", function() {
+    var answerEl = document.createElement("button");
+    answerEl.className = "btn-10";
+    console.log("btn-10 has been clicked")
+});
+
+var buttonEl = document.querySelector("#answer-11");
+var btnEl = document.querySelector("btn-11");
+
+buttonEl.addEventListener("click", function() {
+    var answerEl = document.createElement("button");
+    answerEl.className = "btn-11";
+    console.log("btn-11 has been clicked")
+});
+
+var buttonEl = document.querySelector("#answer-12");
+var btnEl = document.querySelector("btn-12");
+
+buttonEl.addEventListener("click", function() {
+    var answerEl = document.createElement("button");
+    answerEl.className = "btn-12";
+    console.log("btn-12 has been clicked")
+});
+
+var buttonEl = document.querySelector("#answer-13");
+var btnEl = document.querySelector("btn-13");
+
+buttonEl.addEventListener("click", function() {
+    var answerEl = document.createElement("button");
+    answerEl.className = "btn-13";
+    console.log("btn-13 has been clicked")
+});
+
+var buttonEl = document.querySelector("#answer-14");
+var btnEl = document.querySelector("btn-14");
+
+buttonEl.addEventListener("click", function() {
+    var answerEl = document.createElement("button");
+    answerEl.className = "btn-14";
+    console.log("btn-14 has been clicked")
+});
+
+var buttonEl = document.querySelector("#answer-15");
+var btnEl = document.querySelector("btn-15");
+
+buttonEl.addEventListener("click", function() {
+    var answerEl = document.createElement("button");
+    answerEl.className = "btn-15";
+    console.log("btn-15 has been clicked")
+});
+
+var buttonEl = document.querySelector("#answer-16");
+var btnEl = document.querySelector("btn-16");
+
+buttonEl.addEventListener("click", function() {
+    var answerEl = document.createElement("button");
+    answerEl.className = "btn-16";
+    console.log("btn-16 has been clicked")
+});
+
+var buttonEl = document.querySelector("#answer-17");
+var btnEl = document.querySelector("btn-17");
+
+buttonEl.addEventListener("click", function() {
+    var answerEl = document.createElement("button");
+    answerEl.className = "btn-17";
+    console.log("btn-17 has been clicked")
+});
+
+var buttonEl = document.querySelector("#answer-18");
+var btnEl = document.querySelector("btn-18");
+
+buttonEl.addEventListener("click", function() {
+    var answerEl = document.createElement("button");
+    answerEl.className = "btn-18";
+    console.log("btn-18 has been clicked")
+});
+
+var buttonEl = document.querySelector("#answer-19");
+var btnEl = document.querySelector("btn-19");
+
+buttonEl.addEventListener("click", function() {
+    var answerEl = document.createElement("button");
+    answerEl.className = "btn-19";
+    console.log("btn-19 has been clicked")
+});
+
+var buttonEl = document.querySelector("#answer-20");
+var btnEl = document.querySelector("btn-20");
+
+buttonEl.addEventListener("click", function() {
+    var answerEl = document.createElement("button");
+    answerEl.className = "btn-20";
+    console.log("btn-20 has been clicked")
+});
+/* Event Listener Ends */
+
