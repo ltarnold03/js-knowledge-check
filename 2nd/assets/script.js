13781a2eafdaf5b9bcbabca14e31e03b2006b7af
LaMonica_Arnold <ltarnold03@yahoo.com>
2021-05-16 22:18:02 -0500

    js rework on 2nd

---
+++ 2nd/assets/script.js
@@ -0,0 +1,170 @@
+var body = document.body;
+body.setAttribute('style', 'textAlign:center');
+
+var h3 = document.createElement('h3');
+h3.textContent = 'The web storage API setItem method receives which two arguments?';
+h3.setAttribute('style', 'text-align:center;');
+
+body.appendChild(h3);
+
+var btn = document.createElement('button');
+btn.innerHTML = "A.) variables and objects";
+btn.addEventListener("click", function() {
+    console.log("A is clicked");
+})
+document.body.appendChild(btn);
+
+var btn = document.createElement('button');
+btn.innerHTML = "B.) a key-value pair";
+btn.addEventListener("click", function() {
+    console.log("B is clicked");
+})
+document.body.appendChild(btn);
+
+var btn = document.createElement('button');
+btn.innerHTML = "C.) parameter and property";
+btn.addEventListener("click", function() {
+    console.log("C is clicked");
+})
+document.body.appendChild(btn);
+
+var btn = document.createElement('button');
+btn.innerHTML = "D.) none of the above";
+btn.addEventListener("click", function() {
+    console.log("D is clicked");
+})
+document.body.appendChild(btn);
+
+var h3 = document.createElement('h3');
+h3.textContent = 'A data type that can store more then one value, such as an array?';
+h3.setAttribute('style', 'text-align:center;');
+body.appendChild(h3);
+
+var btn = document.createElement('button');
+btn.innerHTML = "A.) primitive";
+btn.addEventListener("click", function() {
+    console.log("A is clicked");
+})
+document.body.appendChild(btn);
+
+var btn = document.createElement('button');
+btn.innerHTML = "B.) objects";
+btn.addEventListener("click", function() {
+    console.log("B is clicked");
+})
+document.body.appendChild(btn);
+
+var btn = document.createElement('button');
+btn.innerHTML = "C.) Booleans";
+btn.addEventListener("click", function() {
+    console.log("C is clicked");
+})
+document.body.appendChild(btn);
+
+var btn = document.createElement('button');
+btn.innerHTML = "D.) variables";
+btn.addEventListener("click", function() {
+    console.log("D is clicked");
+})
+document.body.appendChild(btn);
+
+var h3 = document.createElement('h3');
+h3.textContent = 'Which of the following does not immediatly surround the text of a String?';
+h3.setAttribute('style', 'text-align:center;');
+body.appendChild(h3);
+
+var btn = document.createElement('button');
+btn.innerHTML = "A.) &lsquo; &rsquo;";
+btn.addEventListener("click", function() {
+    console.log("A is clicked");
+})
+document.body.appendChild(btn);
+
+var btn = document.createElement('button');
+btn.innerHTML = "B.) &ldquo; &rdquo;";
+btn.addEventListener("click", function() {
+    console.log("B is clicked");
+})
+document.body.appendChild(btn);
+
+var btn = document.createElement('button');
+btn.innerHTML = "C.) ``";
+btn.addEventListener("click", function() {
+    console.log("C is clicked");
+})
+document.body.appendChild(btn);
+
+var btn = document.createElement('button');
+btn.innerHTML = "D.) ()";
+btn.addEventListener("click", function() {
+    console.log("D is clicked");
+})
+document.body.appendChild(btn);
+
+
+var h3 = document.createElement('h3');
+h3.textContent = 'What is the shorthand for addition in JavaScript?';
+h3.setAttribute('style', 'text-align:center;');
+body.appendChild(h3);
+
+var btn = document.createElement('button');
+btn.innerHTML = "A.) -";
+btn.addEventListener("click", function() {
+    console.log("A is clicked");
+})
+document.body.appendChild(btn);
+
+var btn = document.createElement('button');
+btn.innerHTML = "B.) -=";
+btn.addEventListener("click", function() {
+    console.log("B is clicked");
+})
+document.body.appendChild(btn);
+
+var btn = document.createElement('button');
+btn.innerHTML = "C.) (-)";
+btn.addEventListener("click", function() {
+    console.log("C is clicked");
+})
+document.body.appendChild(btn);
+
+var btn = document.createElement('button');
+btn.innerHTML = "D.) =-";
+btn.addEventListener("click", function() {
+    console.log("D is clicked");
+})
+document.body.appendChild(btn);
+
+var h3 = document.createElement('h3');
+h3.textContent = 'Which of these is the Not operator which is used to switch values?';
+h3.setAttribute('style', 'text-align:center;');
+body.appendChild(h3);
+
+var btn = document.createElement('button');
+btn.innerHTML = "A.) .";
+btn.addEventListener("click", function() {
+    console.log("A is clicked");
+})
+document.body.appendChild(btn);
+
+var btn = document.createElement('button');
+btn.innerHTML = "B.) ++";
+btn.addEventListener("click", function() {
+    console.log("B is clicked");
+})
+document.body.appendChild(btn);
+
+var btn = document.createElement('button');
+btn.innerHTML = "C.) !";
+btn.addEventListener("click", function() {
+    console.log("C is clicked");
+})
+document.body.appendChild(btn);
+
+var btn = document.createElement('button');
+btn.innerHTML = "D.) -";
+btn.addEventListener("click", function() {
+    console.log("D is clicked");
+})
+document.body.appendChild(btn);
+
